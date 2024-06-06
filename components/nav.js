024fd7ac9a5f5b7ee5a405c673e69750d72efecc
prabin bhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-06-06 22:52:47 +0545

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -69,6 +69,15 @@ Highlight
 
 Images
         </Link>
+    
+     <Link href={'/Banner'} className={pathname.includes('/gallery') ? activeLink : inactiveLink}>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 5H19C20.1046 5 21 5.89543 21 7V17C21 18.1046 20.1046 19 19 19H5C3.89543 19 3 18.1046 3 17V7C3 5.89543 3.89543 5 5 5Z" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7 15V11C7 9.89543 7.89543 9 9 9V9C10.1046 9 11 9.89543 11 11V15" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M17 9V15" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M17 12H15.5C13.5 12 13.5 15 15.5 15H17" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M7 13H11" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+
+
+
+Advertisement
+        </Link>
+    
 
         <Link href={'/settings'} className={pathname.includes('/settings') ? activeLink : inactiveLink}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
